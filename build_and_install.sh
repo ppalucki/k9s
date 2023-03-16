667ad206c104196e6855dd0da103075711121d15
Pawel Palucki <pawel.palucki@sap.com>
2023-03-16 23:38:33 +0100

    build_and_install.sh

--- build_and_install.sh
+++ build_and_install.sh
@@ -1,8 +1,11 @@
+https://github.com/derailed/k9s/pull/1647
 
 # disable horizonatl scrool reset on autoscroll
-cp patched_textview.go /home/root/go/pkg/mod/github.com/derailed/tview@v0.7.2/textview.go 
+#cp patched_textview.go /root/go/pkg/mod/github.com/derailed/tview@v0.7.2/textview.go 
+go get github.com/gdamore/tcell/v2
+cat textview.patch | patch /root/go/pkg/mod/github.com/derailed/tview@v0.8.1/textview.go
 
 go build -o k9s main.go
 
-
-ln -sfv $PWD/k9s /usr/local/bin/
+#ln -sfv $PWD/k9s /usr/local/bin/
+ln -sfv $PWD/k9s /root/ppalucki/bin/k9s
